b9cbdbaaeeea05eff0924b419eb68cdc07345bec
Maciej Szurlej <maciekszurlej@wp.pl>
2021-09-29 11:06:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { id: 1, title: "Wiadomość numer 1", body: "Zawartość wiadomości numer1..." },
+  { id: 2, title: "Wiadomość numer 2", body: "Zawartość wiadomości numer2..." },
+];
+
+setInterval(() => {
+  const index = data.length + 1;
+  data.push({
+    id: index,
+    title: `Wiadomość numer ${index}`,
+    body: `Zawartość wiadomości numer ${index}...`,
+  });
+}, 4000);
+
+class App extends Component {
+  state = {
+    comments: [...data],
+  };
+
+  getData = () => {
+    if (this.state.comments.length !== data.length) {
+      console.log("aktualizacja");
+      this.setState({
+        comments: [...data],
+      });
+    } else {
+      console.log("dane takie same - nie aktualizuje");
+    }
+  };
+
+  componentDidMount() {
+    this.idI = setInterval(this.getData, 5000);
+  }
+
+  componentWillUmmount() {
+    clearInterval(this.idI);
+  }
+
+  render() {
+    const comments = this.state.comments.map((comment) => (
+      <div key={comment.id}>
+        <h4>{comment.title}</h4>
+        <div>{comment.body}</div>
+      </div>
+    ));
+    return <div className="App">{comments.reverse()}</div>;
+  }
 }
 
 export default App;
